28b98d7f20868547f8c99a3e4e44394d403b748f
mm0960819-code <mm0960819@gmail.com>
2026-06-28 10:10:23 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 const bcrypt = require('bcrypt');
 const session = require('express-session');
-const rateLimit = require('express-rate-limit');
+
 const db = require('./db');
 
 const app = express();
